37f865b324c6d23edcab3672655cb08340b28b05
Евгений Решетников <122768108+Evgeni2115@users.noreply.github.com>
2023-04-27 13:24:01 +0500

    commit

--- src/main/java/com/example/SpringBootApplication/service/UserServiceImpl.java
+++ src/main/java/com/example/SpringBootApplication/service/UserServiceImpl.java
@@ -2,13 +2,12 @@ package com.example.SpringBootApplication.service;
 
 import com.example.SpringBootApplication.model.User;
 import com.example.SpringBootApplication.repository.UserRepository;
-import jakarta.transaction.Transactional;
+import org.springframework.transaction.annotation.Transactional;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
 
-@Transactional
 @Service
 public class UserServiceImpl implements UserService {
 
@@ -20,21 +19,25 @@ public class UserServiceImpl implements UserService {
     }
 
     @Override
+    @Transactional
     public void saveUser(User user) {
         userRepository.save(user);
     }
 
     @Override
+    @Transactional(readOnly = true)
     public List<User> getAllUsers() {
         return userRepository.findAll();
     }
 
     @Override
+    @Transactional(readOnly = true)
     public User getById(Long id) {
         return userRepository.getById(id);
     }
 
     @Override
+    @Transactional
     public void updateUser(Long id, User user) {
         User existingUser = userRepository.getById(id);
         if (existingUser != null) {
@@ -45,6 +48,7 @@ public class UserServiceImpl implements UserService {
     }
 
     @Override
+    @Transactional
     public void deleteUser(Long id) {
         userRepository.deleteById(id);
     }
